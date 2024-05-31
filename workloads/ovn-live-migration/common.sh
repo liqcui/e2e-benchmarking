efd658e41bcbe70c57e9eec611abcaf5ccf747b3
Liquan Cui <liqcui@redhat.com>
2024-05-31 16:06:08 +0800

    update code to enable ovn live migration

---
+++ workloads/ovn-live-migration/common.sh
@@ -0,0 +1,308 @@
+#!/usr/bin/env bash
+set -m
+# set -x
+source ../../utils/common.sh
+source env.sh
+
+
+openshift_login
+
+# If INDEXING is enabled we retrive the prometheus oauth token
+if [[ ${INDEXING} == "true" ]]; then
+  if [[ ${HYPERSHIFT} == "false" ]]; then
+    export PROM_TOKEN=$(oc create token -n openshift-monitoring prometheus-k8s --duration=6h || oc sa get-token -n openshift-monitoring prometheus-k8s || oc sa new-token -n openshift-monitoring prometheus-k8s)
+  else
+    export PROM_TOKEN="dummytokenforthanos"
+    export HOSTED_CLUSTER_NAME=$(oc get infrastructure cluster -o jsonpath='{.status.infrastructureName}')
+  fi
+fi
+export UUID=${UUID:-$(uuidgen)}
+export OPENSHIFT_VERSION=$(oc version -o json | jq -r '.openshiftVersion') 
+export NETWORK_TYPE=$(oc get network.config/cluster -o jsonpath='{.status.networkType}') 
+export INGRESS_DOMAIN=$(oc get IngressController default -n openshift-ingress-operator -o jsonpath='{.status.domain}' || oc get routes -A --no-headers | head -n 1 | awk {'print$3'} | cut -d "." -f 2-)
+
+platform=$(oc get infrastructure cluster -o jsonpath='{.status.platformStatus.type}')
+
+if [[ ${HYPERSHIFT} == "true" ]]; then
+  # shellcheck disable=SC2143
+  if oc get ns grafana-agent; then
+    log "Grafana agent is already installed"
+  else
+    export CLUSTER_NAME=${HOSTED_CLUSTER_NAME}
+    export PLATFORM=$(oc get infrastructure cluster -o jsonpath='{.status.platformStatus.type}')
+    export DAG_ID=$(oc version -o json | jq -r '.openshiftVersion')-$(oc get infrastructure cluster -o jsonpath='{.status.infrastructureName}') # setting a dynamic value
+    envsubst < ./grafana-agent.yaml | oc apply -f -
+  fi
+  echo "Get all management worker nodes.."
+  export Q_TIME=$(date +"%s")
+  export Q_NODES=""
+  for n in $(curl -k --silent --globoff  ${PROM_URL}/api/v1/query?query='sum(kube_node_role{openshift_cluster_name=~"'${MGMT_CLUSTER_NAME}'",role=~"master|infra|workload"})by(node)&time='$(($Q_TIME-300))'' | jq -r '.data.result[].metric.node'); do
+    Q_NODES=${n}"|"${Q_NODES};
+  done
+  export MGMT_NON_WORKER_NODES=${Q_NODES}
+  # set time for modifier queries 
+  export Q_TIME=$(($Q_TIME+600))
+fi
+
+collect_pprof() {
+  sleep 50
+  while [ $(oc get benchmark -n benchmark-operator kube-burner-${1}-${UUID} -o jsonpath="{.status.complete}") == "false" ]; do
+    log "-----------------------checking for new pprof files--------------------------"
+    oc rsync -n benchmark-operator $(oc get pod -n benchmark-operator -o name -l benchmark-uuid=${UUID}):/tmp/pprof-data $PWD/
+    sleep 60
+  done
+}
+
+run_workload() {
+  local CMD
+  local KUBE_BURNER_DIR 
+  KUBE_BURNER_DIR=$(mktemp -d)
+  if [[ ! -d ${KUBE_DIR} ]]; then
+    mkdir -p ${KUBE_DIR}
+  fi
+  if [[ -n ${BUILD_FROM_REPO} ]]; then
+    git clone --depth=1 ${BUILD_FROM_REPO} ${KUBE_BURNER_DIR}
+    make -C ${KUBE_BURNER_DIR} build
+    mv ${KUBE_BURNER_DIR}/bin/amd64/kube-burner ${KUBE_DIR}/kube-burner
+    rm -rf ${KUBE_BURNER_DIR}
+  else
+    curl -sS -L ${KUBE_BURNER_URL} | tar -xzC ${KUBE_DIR}/ kube-burner
+  fi
+  CMD="timeout ${JOB_TIMEOUT} ${KUBE_DIR}/kube-burner init --uuid=${UUID} -c $(basename ${WORKLOAD_TEMPLATE}) --log-level=${LOG_LEVEL}"
+
+  # When metrics or alerting are enabled we have to pass the prometheus URL to the cmd
+  if [[ ${INDEXING} == "true" ]] || [[ ${PLATFORM_ALERTS} == "true" ]] ; then
+    CMD+=" -u=${PROM_URL} -t ${PROM_TOKEN}"
+  fi
+  if [[ -n ${METRICS_PROFILE} ]]; then
+    log "Indexing enabled, using metrics from ${METRICS_PROFILE}"
+    envsubst < ${METRICS_PROFILE} > ${KUBE_DIR}/metrics.yml
+    CMD+=" -m ${KUBE_DIR}/metrics.yml"
+  fi
+  if [[ ${PLATFORM_ALERTS} == "true" ]]; then
+    log "Platform alerting enabled, using ${PWD}/alerts-profiles/${WORKLOAD}-${platform}.yml"
+    CMD+=" -a ${PWD}/alerts-profiles/${WORKLOAD}-${platform}.yml"
+  fi
+  pushd $(dirname ${WORKLOAD_TEMPLATE})
+  local start_date=$(date +%s%3N)
+  ${CMD}
+  rc=$?
+  popd
+  if [[ ${rc} == 0 ]]; then
+    RESULT=Complete
+  else
+    RESULT=Failed
+  fi
+  gen_metadata ${WORKLOAD} ${start_date} $(date +%s%3N)
+}
+
+find_running_pods_num() {
+  pod_count=0
+  # The next statement outputs something similar to:
+  # ip-10-0-177-166.us-west-2.compute.internal:20
+  # ip-10-0-250-197.us-west-2.compute.internal:17
+  # ip-10-0-151-0.us-west-2.compute.internal:19
+  NODE_PODS=$(kubectl get pods --field-selector=status.phase=Running -o go-template --template='{{range .items}}{{.spec.nodeName}}{{"\n"}}{{end}}' -A | awk '{nodes[$1]++ }END{ for (n in nodes) print n":"nodes[n]}')
+  for worker_node in ${WORKER_NODE_NAMES}; do
+    for node_pod in ${NODE_PODS}; do
+      # We use awk to match the node name and then we take the number of pods, which is the number after the colon
+      pods=$(echo "${node_pod}" | awk -F: '/'$worker_node'/{print $2}')
+      pod_count=$((pods + pod_count))
+    done
+  done
+  log "Total running pods across nodes: ${pod_count}"
+  # Number of pods to deploy per node * number of labeled nodes - pods running
+  total_pod_count=$((PODS_PER_NODE * NODE_COUNT - pod_count))
+  log "Number of pods to deploy on nodes: ${total_pod_count}"
+  if [[ ${1} == "heavy" ]] || [[ ${1} == *cni* ]]; then
+    total_pod_count=$((total_pod_count / 2))
+  fi
+  if [[ ${total_pod_count} -le 0 ]]; then
+    log "Number of pods to deploy <= 0"
+    exit 1
+  fi
+  export TEST_JOB_ITERATIONS=${total_pod_count}
+}
+
+cleanup() {
+  log "Cleaning up benchmark assets"
+  if ! oc delete ns -l kube-burner-uuid=${UUID} --grace-period=600 --timeout=${CLEANUP_TIMEOUT} 1>/dev/null; then
+    log "Namespaces cleanup failure"
+    rc=1
+  fi
+}
+
+get_pprof_secrets() {
+  if [[ ${HYPERSHIFT} == "true" ]]; then
+    log "Control Plane not available in HyperShift"
+    exit 1
+  else
+    oc create ns benchmark-operator
+    oc create serviceaccount kube-burner -n benchmark-operator
+    oc create clusterrolebinding kube-burner-crb --clusterrole=cluster-admin --serviceaccount=benchmark-operator:kube-burner
+    local certkey=`oc get secret -n openshift-etcd | grep "etcd-serving-ip" | head -1 | awk '{print $1}'`
+    oc extract -n openshift-etcd secret/$certkey
+    export CERTIFICATE=`base64 -w0 tls.crt`
+    export PRIVATE_KEY=`base64 -w0 tls.key`
+    export BEARER_TOKEN=$(oc create token -n benchmark-operator kube-burner --duration=6h || oc sa get-token kube-burner -n benchmark-operator)
+  fi
+}
+
+delete_pprof_secrets() {
+ rm -f tls.key tls.crt
+}
+
+delete_oldpprof_folder() {
+ rm -rf pprof-data
+}
+
+label_node_with_label() {
+  colon_param=$(echo $1 | tr "=" ":" | sed 's/:/: /g')
+  export POD_NODE_SELECTOR="{$colon_param}"
+  if [[ -z $NODE_COUNT ]]; then
+    NODE_COUNT=$(oc get node -o name --no-headers -l ${WORKER_NODE_LABEL},node-role.kubernetes.io/infra!=,node-role.kubernetes.io/workload!= | wc -l )
+  fi
+  if [[ ${NODE_COUNT} -le 0 ]]; then
+    log "Node count <= 0: ${NODE_COUNT}"
+    exit 1
+  fi
+  WORKER_NODE_NAMES=$(oc get node -o custom-columns=name:.metadata.name --no-headers -l ${WORKER_NODE_LABEL},node-role.kubernetes.io/infra!=,node-role.kubernetes.io/workload!= | head -n ${NODE_COUNT})
+  if [[ $(echo "${WORKER_NODE_NAMES}" | wc -l) -lt ${NODE_COUNT} ]]; then
+    log "Not enough worker nodes to label"
+    exit 1
+  fi
+
+  log "Labeling ${NODE_COUNT} worker nodes with $1"
+  oc label node ${WORKER_NODE_NAMES} $1 --overwrite 1>/dev/null
+}
+
+unlabel_nodes_with_label() {
+  split_param=$(echo $1 | tr "=" " ")
+  log "Removing $1 label from worker nodes"
+  for worker_node in ${WORKER_NODE_NAMES}; do
+    for p in ${split_param}; do
+      oc label node $worker_node $p- 1>/dev/null
+      break
+    done
+  done
+}
+
+prep_networkpolicy_workload() {
+  export ES_INDEX_NETPOL=${ES_INDEX_NETPOL:-networkpolicy-enforcement}
+  oc apply -f workloads/networkpolicy/clusterrole.yml
+  oc apply -f workloads/networkpolicy/clusterrolebinding.yml
+}
+
+function verify_if_mcp_be_in_updated_state_by_name() {
+	
+    MCP_NAME=$1
+		mcpUpdatingStatus=`oc get mcp $MCP_NAME -ojsonpath='{.status.conditions[?(@.type=="Updating")].status}'`
+		mcpUpdatedStatus=`oc get mcp $MCP_NAME -ojsonpath='{.status.conditions[?(@.type=="Updated")].status}'`
+
+		mcpMachineCount=`oc get mcp $MCP_NAME -ojsonpath={..status.machineCount}`
+		mcpReadyMachineCount=`oc get mcp $MCP_NAME -ojsonpath={..status.readyMachineCount}`
+		mcpUpdatedMachineCount=`oc get mcp $MCP_NAME -ojsonpath={..status.updatedMachineCount}`
+		mcpDegradedMachineCount=`oc get mcp $MCP_NAME -ojsonpath={..status.degradedMachineCount}`
+		if [[ $mcpUpdatingStatus=="False" && $mcpUpdatedStatus=="True" && $mcpMachineCount == $mcpReadyMachineCount && $mcpMachineCount == $mcpUpdatedMachineCount && $mcpDegradedMachineCount == "0" ]];then
+        echo true
+    else
+        echo false
+    fi
+}
+
+function live-migration-keepalive-detect(){
+    LIVE_MIGRATION_DETECT_INTERVAL=${LIVE_MIGRATION_DETECT_INTERVAL:=1}
+    INIT=1
+    MAX_RETRY=${MAX_RETRY:=7200}  
+    oc get mcp |grep infra>/dev/null
+    RC1=$?
+    oc get mcp |grep workload>/dev/null
+    RC2=$?
+    echo $RC1 $RC2
+    if [[ $RC1 -ne 0 || $RC2 -ne 0 ]];then
+        echo please enable infra and workload node
+        exit 1
+    fi
+
+    if ! oc get route -A |grep keepalive-detectd-nginx-service; then
+        echo "Create a route service to public network"
+        oc -n ovn-live-migration-1 create route edge --service=keepalive-detectd-nginx-service
+    fi
+    echo "Start to OVN live migration ...."
+    awk 'BEGIN{for(c=0;c<80;c++) printf "-"; printf "\n"}'
+    oc patch Network.config.openshift.io cluster --type='merge' --patch '{"metadata":{"annotations":{"unsupported-red-hat-internal-testing": "true"}}}'
+    oc patch Network.config.openshift.io cluster --type='merge' --patch '{"metadata":{"annotations":{"network.openshift.io/network-type-migration":""}},"spec":{"networkType":"OVNKubernetes"}}'
+    awk 'BEGIN{for(c=0;c<80;c++) printf "-"; printf "\n"}'
+    echo "Start to delect if the service broken during OVN live migration ...."
+    
+    while true;
+    do
+                curl -k -Is https://keepalive-detectd-nginx-service-ovn-live-migration-1.apps.liqcui-oc414sdn.qe.gcp.devcluster.openshift.com/ | head -n 1| grep OK;
+                RC=$?;
+                if [ $RC -eq 0 ] ; then 
+                   echo Service detect succesfully during ovn live migration.
+                else
+                   awk 'BEGIN{for(c=0;c<80;c++) printf "#"; printf "\n"}'
+                   echo "#       [Failure] Service broken during ovn live migration at `date +"%Y-%m-%d %H:%M:%S"`      #"
+                   awk 'BEGIN{for(c=0;c<80;c++) printf "#"; printf "\n"}'
+                fi;
+                NETWORK_TYPE=`oc get network.config.openshift.io cluster -o jsonpath='{.status.networkType}'`
+    
+                if [[ $NETWORK_TYPE == "OVNKubernetes" ]];then
+                #if [[ $NETWORK_TYPE == "OpenShiftSDN" ]];then
+                   awk 'BEGIN{for(c=0;c<80;c++) printf "#"; printf "\n"}'
+                   echo "#       The OCP Network Type Changed to NETWORK_TYPE at `date +"%Y-%m-%d %H:%M:%S"`         #"
+                   echo "#                     Current Network Type is $NETWORK_TYPE                    #"
+                   awk 'BEGIN{for(c=0;c<80;c++) printf "#"; printf "\n"}'     
+                   break
+                fi
+                sleep $LIVE_MIGRATION_DETECT_INTERVAL;
+                INIT=$(( $INIT + 1 ))
+                if [[ $INIT -gt $MAX_RETRY ]];then
+                   echo "max retry reached in live-migration-post-check"
+                   exit 1
+                fi
+    done
+}
+
+
+function live-migration-post-check(){
+    INIT=1
+    MAX_RETRY=${MAX_RETRY:=7200}
+    echo "Start to delect if the service broken during the second reboot of OVN live migration ...."
+    
+    
+    while true;
+    do
+                curl -k -Is https://keepalive-detectd-nginx-service-ovn-live-migration-1.apps.liqcui-oc414sdn.qe.gcp.devcluster.openshift.com/ | head -n 1| grep OK;
+                RC=$?;
+                if [ $RC -eq 0 ] ; then 
+                   echo Service detect succesfully during ovn live migration.
+                else
+                   awk 'BEGIN{for(c=0;c<80;c++) printf "#"; printf "\n"}'
+                   echo "#       [Failure] Service broken during ovn live migration at `date +"%Y-%m-%d %H:%M:%S"`      #"
+                   awk 'BEGIN{for(c=0;c<80;c++) printf "#"; printf "\n"}'
+                fi;
+               
+                MASTER_MCP_STATUS=`verify_if_mcp_be_in_updated_state_by_name master`
+                WORKER_MCP_STATUS=`verify_if_mcp_be_in_updated_state_by_name worker`
+                INFRA_MCP_STATUS=`verify_if_mcp_be_in_updated_state_by_name infra`
+                WORKLOAD_MCP_STATUS=`verify_if_mcp_be_in_updated_state_by_name workload`
+    
+                if [[ $MASTER_MCP_STATUS == "true" && $MASTER_MCP_STATUS == "true" && $INFRA_MCP_STATUS == "true" && $WORKLOAD_MCP_STATUS == "true" ]];then
+                #if [[ $NETWORK_TYPE == "OpenShiftSDN" ]];then
+                   awk 'BEGIN{for(c=0;c<80;c++) printf "#"; printf "\n"}'
+                   echo "#            OVN Live Migration Successfully at `date +"%Y-%m-%d %H:%M:%S"`            #"
+                   echo "#                     Current Network Type is $NETWORK_TYPE                    #"
+                   awk 'BEGIN{for(c=0;c<80;c++) printf "#"; printf "\n"}'     
+                   break
+                fi
+                sleep 5;
+                INIT=$(( $INIT + 1 ))
+                if [[ $INIT -gt $MAX_RETRY ]];then
+                   echo "max retry reached in live-migration-post-check"
+                   exit 1
+                fi
+    done
+}
+    
