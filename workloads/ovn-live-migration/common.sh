e26e31b9597186ba0ef3b6e5c89c0290e3e1b4eb
Liquan Cui <liqcui@redhat.com>
2024-06-01 11:17:03 +0800

    update code to enable ovn live migration

--- workloads/ovn-live-migration/common.sh
+++ workloads/ovn-live-migration/common.sh
@@ -265,6 +265,72 @@ function live-migration-keepalive-detect(){
     done
 }
 
+function cluster_health_postcheck() {
+ 
+  #target_version_prefix=$1
+  echo -e "**************Post Action after upgrade succ****************\n"
+  echo -----------------------------------------------------------------------
+  echo -e "Post action: #oc get node:\n"
+  oc get node -o wide
+  echo -----------------------------------------------------------------------
+  echo
+  echo -e "Post action: #oc get co:\n"
+  echo -----------------------------------------------------------------------
+  oc get co
+  echo -----------------------------------------------------------------------
+
+  echo -e "print detail msg for node(SchedulingDisabled) if exist:\n"
+  echo -e "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Abnormal node details~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n"
+  nodeStatusCheckResult=${nodeStatusCheckResult:=""}
+  if oc get node --no-headers | grep -E 'SchedulingDisabled|NotReady' ; then
+                  oc get node --no-headers | grep -E 'SchedulingDisabled|NotReady'| awk '{print $1}'|while read line; do oc describe node $line;done
+                  nodeStatusCheckResult="abnormal"
+  fi
+  echo
+  echo -e "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n"
+  echo -e "print detail msg for co(AVAILABLE != True or PROGRESSING!=False or DEGRADED!=False or version != target_version) if exist:\n"
+  # Check if the kube-apiserver is rolling out after upgrade
+  if [ -z "$nodeStatusCheckResult" ]; then # If master nodes are normal
+      kas_rollingout_wait
+  fi 
+  echo nodeStatusCheckResult is $nodeStatusCheckResult
+  echo -e "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Abnormal co details~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n"
+
+  abnormalCO=${abnormalCO:=""}
+  echo abnormalCO is $abnormalCO
+  ! oc get co -o jsonpath='{range .items[*]}{.metadata.name} {range .status.conditions[*]} {.type}={.status}{end}{"\n"}{end}' | grep -v "openshift-samples" | grep -w -E 'Available=False|Progressing=True|Degraded=True' || abnormalCO=`oc get co -o jsonpath='{range .items[*]}{.metadata.name} {range .status.conditions[*]} {.type}={.status}{end}{"\n"}{end}' | grep -v "openshift-samples" | grep -w -E 'Available=False|Progressing=True|Degraded=True' | awk '{print $1}'`
+  echo "abnormalCO is $abnormalCO before quick_diagnosis"
+
+  if [[ "X${abnormalCO}" != "X" ]]; then
+      echo "Start quick_diagnosis"
+      quick_diagnosis "$abnormalCO"
+      for aco in $abnormalCO; do
+          oc describe co $aco
+          echo -e "\n~~~~~~~~~~~~~~~~~~~~~~~\n"
+      done
+  fi
+  echo abnormalCO is $abnormalCO after quick_diagnosis
+  coStatusCheckResult=${coStatusCheckResult:=""}
+ ! oc get co |sed '1d'|grep -v "openshift-samples"|grep -v "True        False         False" || coStatusCheckResult=`oc get co |sed '1d'|grep -v "openshift-samples"|grep -v "True        False         False"|awk '{print $1}'|while read line; do oc describe co $line;done`
+  echo coStatusCheckResult is $coStatusCheckResult 
+  echo -e "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n"
+
+  coVersionCheckResult=${coVersionCheckResult:=""}
+  ! oc get co |sed '1d'|grep -v "openshift-samples"|grep -v ${target_version_prefix} || coVersionCheckResult=`oc get co |sed '1d'|grep -v "openshift-samples"|grep -v ${target_version_prefix}|awk '{print $1}'|while read line; do oc describe co $line;done`
+  echo coVersionCheckResult is $coVersionCheckResult
+  echo -e "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n"
+
+
+  if [ -z "$nodeStatusCheckResult" ] && [ -z "$coStatusCheckResult" ] && [ -z "$coVersionCheckResult" ]; then
+      echo -e "post check passed without err.\n"
+
+  else
+      oc get nodes
+      oc describe nodes
+  fi
+
+}
+
 
 function live-migration-post-check(){
     INIT=1
@@ -304,5 +370,6 @@ function live-migration-post-check(){
                    exit 1
                 fi
     done
+    cluster_health_postcheck
 }
     
