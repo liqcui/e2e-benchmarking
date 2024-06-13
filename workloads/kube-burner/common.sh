c5222dcad6f6c7117e1e9befd41cd16dc7c9b2b9
Liquan Cui <liqcui@redhat.com>
2024-06-13 18:39:51 +0800

    update code

--- workloads/kube-burner/common.sh
+++ workloads/kube-burner/common.sh
@@ -1602,7 +1602,6 @@ function restartOVNPODs(){
       awk 'BEGIN{for(c=0;c<80;c++) printf "-"; printf "\n"}'
       echo "Restart OVN Pods"
       OVN_NODE_POD_NAMES=`oc -n openshift-ovn-kubernetes  get pods |grep -v -i -E 'ovnkube-control-plane|NAME' | awk '{print $1}'| tail -5`
-      echo ovn_node_pod
       for ovn_node_pod in $OVN_NODE_POD_NAMES
       do  
            echo restart pod $ovn_node_pod
