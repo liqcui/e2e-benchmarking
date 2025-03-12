f713ddfe813fd0d08267918120f6d4a654516f9b
liqcui <liqcui@redhat.com>
2025-03-12 17:10:47 +0800

    update code

--- workloads/sdn-ovn-migration/run.sh
+++ workloads/sdn-ovn-migration/run.sh
@@ -229,8 +229,11 @@ EOF
                   sed -i 's/replicas: 3/replicas: 11/' cluster-density-v2.yml
                   sed -i 's/replicas: 2/replicas: 3/' cluster-density-v2.yml
                   sed -i 's/replicas: 11/replicas: 2/' cluster-density-v2.yml
+                  #service number
                   sed -i 's/replicas: 5/replicas: 2/' cluster-density-v2.yml
-                  sed -i 's/replicas: 10/replicas: 5/' cluster-density-v2.yml                  
+                  sed -i 's/replicas: 10/replicas: 5/' cluster-density-v2.yml
+                  #service number
+                  sed -i 's/randInt 1 6/randInt 1 2/'  deployment-client.yml                
                   # #Remove Network Policy
                   # sed -i '/np-deny-all.yml/, +9d' cluster-density-v2.yml
                   # sed -i '/np-deny-all.yml/{N;N;d;}' cluster-density-v2.yml
