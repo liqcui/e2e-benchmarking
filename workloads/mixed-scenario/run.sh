fee0de53e3f600a3a663fc0561f0023f52905705
liqcui <liqcui@redhat.com>
2025-03-21 17:16:41 +0800

    update code

--- workloads/mixed-scenario/run.sh
+++ workloads/mixed-scenario/run.sh
@@ -416,28 +416,28 @@ EOF
         echo "Query metric from prometheus and compare the result ..."
         awk 'BEGIN{for(c=0;c<80;c++) printf "-"; printf "\n"}'
         python3 -m pip install elasticsearch
-        python3 ./get-ovn-metrics.py -q "topk(10, ovnkube_controller_ready_duration_seconds)" -s $JOB_START -e $JOB_END | tee ovn_metric_result.txt
+        python3 get-ovn-metrics.py -q "topk(10, ovnkube_controller_ready_duration_seconds)" -s $JOB_START -e $JOB_END | tee ovn_metric_result.txt
         maxValue=`cat ovn_metric_result.txt |grep No.1| awk -F',' '{print  $3}'| tr -d ' '`
         IntValue=$(echo "$maxValue" | cut -d. -f1)
         if [ $IntValue -gt 100 ];then
             echo "The max value of ovnkube_controller_ready_duration_seconds is great than expected value"
             exit 1
         fi
-        python3 ./get-ovn-metrics.py -q "topk(10, ovnkube_node_ready_duration_seconds)" -s $JOB_START -e $JOB_END | tee ovn_metric_result.txt
+        python3 get-ovn-metrics.py -q "topk(10, ovnkube_node_ready_duration_seconds)" -s $JOB_START -e $JOB_END | tee ovn_metric_result.txt
         maxValue=`cat ovn_metric_result.txt |grep No.1| awk -F',' '{print  $3}'| tr -d ' '`
         IntValue=$(echo "$maxValue" | cut -d. -f1)
         if [ $IntValue -gt 100 ];then
             echo "The max value of ovnkube_node_ready_duration_seconds is great than expected value"
             exit 1
         fi
-        python3 ./get-ovn-metrics.py -q "topk(10, ovnkube_controller_sync_duration_seconds)" -s $JOB_START -e $JOB_END | tee ovn_metric_result.txt
+        python3 get-ovn-metrics.py -q "topk(10, ovnkube_controller_sync_duration_seconds)" -s $JOB_START -e $JOB_END | tee ovn_metric_result.txt
         maxValue=`cat ovn_metric_result.txt |grep No.1| awk -F',' '{print  $3}'| tr -d ' '`
         IntValue=$(echo "$maxValue" | cut -d. -f1)
         if [ $IntValue -gt 100 ];then
             echo "The max value of ovnkube_controller_sync_duration_seconds is great than expected value"
             exit 1
         fi
-        python3 ./get-ovn-metrics.py -q "histogram_quantile(0.9, sum by(pod, event, le) (rate(ovnkube_controller_pod_event_latency_seconds_bucket[5m])))" -s $JOB_START -e $JOB_END | tee ovn_metric_result.txt
+        python3 get-ovn-metrics.py -q "histogram_quantile(0.9, sum by(pod, event, le) (rate(ovnkube_controller_pod_event_latency_seconds_bucket[5m])))" -s $JOB_START -e $JOB_END | tee ovn_metric_result.txt
         maxValue=`cat ovn_metric_result.txt |grep No.1| awk -F',' '{print  $3}'| tr -d ' '`
         IntValue=$(echo "$maxValue" | cut -d. -f1)
         if [ $IntValue -gt 100 ];then
