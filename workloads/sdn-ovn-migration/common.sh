adf4d6d3c3467ca2c15731702cf40f041069c7fc
liqcui <liqcui@redhat.com>
2025-05-11 19:05:53 +0800

    update code

--- workloads/sdn-ovn-migration/common.sh
+++ workloads/sdn-ovn-migration/common.sh
@@ -1333,6 +1333,7 @@ function post_check_after_migration(){
           START_TIME=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
           echo
           awk 'BEGIN{for(c=0;c<80;c++) printf "#"; printf "\n"}' 
+          awk 'BEGIN{for(c=0;c<80;c++) printf "-"; printf "\n"}'           
           echo "Check OVN Pods Status"
           awk 'BEGIN{for(c=0;c<80;c++) printf "-"; printf "\n"}'       
           oc -n openshift-ovn-kubernetes get pods
