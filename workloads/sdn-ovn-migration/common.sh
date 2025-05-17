dd04ca34a811318761f55923fb89807e47f5a933
liqcui <liqcui@redhat.com>
2025-05-17 22:35:55 +0800

    update code

--- workloads/sdn-ovn-migration/common.sh
+++ workloads/sdn-ovn-migration/common.sh
@@ -1326,6 +1326,12 @@ function post_check_after_migration(){
     DETECT_INTERVAL=${DETECT_INTERVAL:=30}
     
     echo The max retry is $MAX_RETRY
+    echo "Object numbers of ETCD"
+    export ETCD_POD_NAME=$(oc get pods -n openshift-etcd -l app=etcd --field-selector="status.phase==Running" -o jsonpath="{.items[0].metadata.name}")
+    oc exec -n openshift-etcd ${ETCD_POD_NAME} -- bash -c "etcdctl get / --prefix --keys-only | sed '/^$/d' | cut -d/ -f3 | sort | uniq -c | sort -rn"
+    # echo "Object Size of ETCD"
+    # oc exec -n openshift-etcd -c etcdctl ${ETCD_POD_NAME} -- sh -c "etcdctl get / --prefix --keys-only  | grep -oE '^/[a-z|.]+/[a-z|.|8]*' | sort | uniq -c | sort -rn" | while read KEY; do printf "$KEY\t" && oc exec -n openshift-etcd ${ETCD_POD_NAME} -c etcdctl -- etcdctl get ${KEY##* } --prefix --write-out=json | jq '[.kvs[].value | length] | add ' | numfmt --to=iec ; done | sort -k3 -hr | column -t
+
     while true;
     do
           START_TIME=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
