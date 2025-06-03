3c6621c8950cce77574995a3f4d2ed806b17199d
liqcui <liqcui@redhat.com>
2025-06-04 07:28:50 +0800

    update code

--- workloads/sdn-ovn-migration/run.sh
+++ workloads/sdn-ovn-migration/run.sh
@@ -230,9 +230,9 @@ EOF
                   sed -i 's/replicas: 2/replicas: 3/' cluster-density-v2.yml
                   sed -i 's/replicas: 11/replicas: 2/' cluster-density-v2.yml
                   #service number
-                  sed -i 's/replicas: 5/replicas: 3/' cluster-density-v2.yml
+                  sed -i 's/replicas: 5/replicas: 5/' cluster-density-v2.yml
                   #service number
-                  sed -i 's/randInt 1 6/randInt 1 3/'  deployment-client.yml
+                  sed -i 's/randInt 1 6/randInt 1 5/'  deployment-client.yml
 
                   #secret/configmap number 
                   # sed -i 's/replicas: 10/replicas: 8/' cluster-density-v2.yml
