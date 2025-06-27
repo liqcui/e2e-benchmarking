e81c52722628bd9fe7f61aa7e23397c59b0b7a1e
liqcui <liqcui@redhat.com>
2025-06-27 13:42:53 +0800

    update code

--- workloads/sdn-ovn-migration/common.sh
+++ workloads/sdn-ovn-migration/common.sh
@@ -646,7 +646,11 @@ function sdn-ovn-live-migration-keepalive-detect-phaseI(){
     echo "Start to OVN live migration ...."
     awk 'BEGIN{for(c=0;c<80;c++) printf "-"; printf "\n"}'
     #oc patch Network.config.openshift.io cluster --type='merge' --patch '{"metadata":{"annotations":{"unsupported-red-hat-internal-testing": "true"}}}'
-    oc patch Network.config.openshift.io cluster --type='merge' --patch '{"metadata":{"annotations":{"network.openshift.io/network-type-migration":""}},"spec":{"networkType":"OVNKubernetes"}}'
+    
+    #Manually Execute
+    echo oc patch Network.config.openshift.io cluster --type='merge' --patch '{"metadata":{"annotations":{"network.openshift.io/network-type-migration":""}},"spec":{"networkType":"OVNKubernetes"}}'
+    #oc patch Network.config.openshift.io cluster --type='merge' --patch '{"metadata":{"annotations":{"network.openshift.io/network-type-migration":""}},"spec":{"networkType":"OVNKubernetes"}}'
+    
     #4.15 oc patch Network.operator.openshift.io cluster --type='merge' --patch '{ "spec": { "migration": {"networkType": "OVNKubernetes" } } }'
     awk 'BEGIN{for(c=0;c<80;c++) printf "-"; printf "\n"}'
     export NW_MIGRATION_START=$(date +%s)
