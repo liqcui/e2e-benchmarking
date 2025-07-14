e8bc24c66127b2e0dca9027bd6fa957b5ae53dd0
liqcui <liqcui@redhat.com>
2025-07-14 11:30:48 +0800

    update code

--- workloads/sdn-ovn-migration/run.sh
+++ workloads/sdn-ovn-migration/run.sh
@@ -217,7 +217,7 @@ JOB_START: ${JOB_START}
 EOF
               create_api_flowcontrol
               cd customized-workload
-              #EGRESS_FIREWALL_POLICY_TEMPLAT_FILE_PATH=./egress-firewall-policy.yml
+              EGRESS_FIREWALL_POLICY_TEMPLAT_FILE_PATH=./egress-firewall-policy.yml
               generated_egress_firewall_policy $EGRESS_FIREWALL_POLICY_RULES_TOTAL_NUM
               cat ./egress-firewall-policy.yml
               
